59180b2cb0248ea89521afc8ed5cdadbb77e6103
alameen404-ship-it <alameen404@gmail.com>
2026-04-27 20:58:26 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,22 @@ const qrcode = require('qrcode-terminal');
 const pino = require('pino');
 const http = require('http');
 
+// ... (الأكواد السابقة في الأعلى تبقى كما هي)
+
 async function connectToWhatsApp() {
     const { state, saveCreds } = await useMultiFileAuthState('auth_info');
     
     const sock = makeWASocket({
         auth: state,
-        logger: pino({ level: 'silent' }) // إخفاء التحذيرات المزعجة
+        logger: pino({ level: 'silent' }),
+        // إضافة تعريف المتصفح لتجنب الحظر أو قطع الاتصال
+        browser: ["Fast Bird Gateway", "Chrome", "114.0.5735.199"],
+        connectTimeoutMs: 60000, // زيادة وقت انتظار الاتصال لـ 60 ثانية
+        defaultQueryTimeoutMs: 0,
+        keepAliveIntervalMs: 10000 // الحفاظ على النبض كل 10 ثوانٍ
     });
 
+// ... (بقية الكود المستمع للاتصال)
     sock.ev.on('creds.update', saveCreds);
 
     sock.ev.on('connection.update', (update) => {
